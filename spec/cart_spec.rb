035cb9f4b71cfb5fda20c49cb8cb990b6c9bebc8
Tony Lin <tonylin1208@gmail.com>
2018-06-17 16:09:22 +0800

    finish the course

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -41,21 +41,5 @@ describe Cart do
     end
   end
 
-  context "第三種情境：三本不同 打10%折扣" do
-    it '第一集買一本 第二集買一本 第三集買一本' do
-      @cart.add({ "1st": 1, "2nd": 1, "3rd": 1, "4th": 0, "5th": 0})
-      expect(@cart.calculate).to eq(270)
-    end
-
-    it '第一集買兩本 第二集買兩本 第四集買兩本' do
-      @cart.add({ "1st": 2, "2nd": 2, "3rd": 0, "4th": 2, "5th": 0})
-      expect(@cart.calculate).to eq(540)
-    end
-
-    it '第一集買一本 第二集買兩本 第五集買四本' do
-      @cart.add({ "1st": 1, "2nd": 2, "3rd": 0, "4th": 0, "5th": 4})
-      expect(@cart.calculate).to eq(270+190+200)
-    end
-
-  end
+  
 end
